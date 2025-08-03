50ea7f6399f5d9ae84903dfc1bf9bdddbdb08da2
WebbizualTech <webbizualtech25@gmail.com>
2025-08-03 20:12:40 +0530

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 // App.tsx
+import { HashRouter as Router } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <HomePage />
+    </Router>
+  );
 }
 
 export default App;
